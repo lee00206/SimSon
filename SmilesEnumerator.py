5a99b81486229bd112837b7195d7a3390d16af62
ChaeEun Lee <taylor.lee1017@gmail.com>
2025-05-13 11:00:32 +0900

    Add references

--- SmilesEnumerator.py
+++ SmilesEnumerator.py
@@ -1,4 +1,5 @@
-#Experimental Class for Smiles Enumeration, Iterator and SmilesIterator adapted from Keras 1.2.2
+# Code adopted from: https://github.com/EBjerrum/SMILES-enumeration
+# Experimental Class for Smiles Enumeration, Iterator and SmilesIterator adapted from Keras 1.2.2
 from rdkit import Chem
 import numpy as np
 import threading
